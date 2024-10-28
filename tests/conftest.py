f94bbafdebd76f4e1d4701fe58f4529775373ab9
Mdrahil1911 <mdrahil1911@gmail.com>
2024-10-28 17:40:25 +0530

    added code for running them in headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-import os
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="session")
-def driver():
-    driver = webdriver.Chrome()
+# def driver():
+#     driver = webdriver.Chrome()
+#
+#     yield driver
+#     driver.quit()
 
-    yield driver
-    driver.quit()
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
