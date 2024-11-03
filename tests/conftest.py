d1b5b27b6344482e7273525d876cb2c3b66bc8e3
Mdrahil1911 <mdrahil1911@gmail.com>
2024-11-03 22:19:26 +0530

    headless

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,20 +6,21 @@ from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 
 @pytest.fixture(scope="session")
-def driver():
-    driver = webdriver.Chrome()
-
-    yield driver
-    driver.quit()
-
 # def driver():
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-#     chrome_options.add_argument("--no-sandbox")
-#     chrome_options.add_argument("--disable-dev-shm-usage")
+#     driver = webdriver.Chrome()
 #
-#     driver = webdriver.Chrome(options=chrome_options)
-#     return driver
+#     yield driver
+#     driver.quit()
+
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
 
 BASE_URL = "https://admin.nfttrace.com/login"
 USERNMAE = "superadmin@gmail.com"
