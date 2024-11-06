ccfdd256c74bc62b55ab0e26fecea93827ee3c41
Mdrahil1911 <mdrahil1911@gmail.com>
2024-11-06 09:42:59 +0530

    categories locator issue resolved

--- pages/categories_pages.py
+++ pages/categories_pages.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 
 class Categories(BasePage):
@@ -10,5 +12,7 @@ class Categories(BasePage):
         self.click(self.CATEGORIES)
 
     def get_categories_title(self):
-        transaction_title_element = self.driver.find_element(*self.TITTLE)
-        return transaction_title_element.text
+        categories_title_element = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.TITTLE)
+        )
+        return categories_title_element.text
